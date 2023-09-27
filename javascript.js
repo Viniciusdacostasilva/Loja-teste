e2ea83dfdefd7da4f8f4c4812f1a666a0f9b5a99
Viniciusdacostasilva <135259286+Viniciusdacostasilva@users.noreply.github.com>
2023-09-26 23:26:11 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,12 +1,3 @@
-
- // Adiciona um evento de clique a todos os botões "Mais Detalhes"
- const buttons = document.querySelectorAll('.card-button');
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        openModal(button.nextElementSibling); // Abre o modal que segue o botão
-    });
-    
-});
 const image1 = document.getElementById("image1");
 const image2 = document.getElementById("image2");
 
@@ -19,3 +10,24 @@ image2.addEventListener("mouseleave", () => {
     image1.style.display = "block";
     image2.style.display = "none";
 });
+
+
+
+const defaultImage = document.querySelector(".default-image");
+
+defaultImage.addEventListener("mousemove", (e) => {
+    const { left, top, width, height } = defaultImage.getBoundingClientRect();
+    const x = e.clientX - left;
+    const y = e.clientY - top;
+    const centerX = width / 2;
+    const centerY = height / 2;
+    const deltaX = (x - centerX) / centerX;
+    const deltaY = (y - centerY) / centerY;
+
+    defaultImage.style.transform = `scale(1.2) translate(${deltaX * 10}px, ${deltaY * 10}px)`;
+});
+
+defaultImage.addEventListener("mouseleave", () => {
+    defaultImage.classList.remove("zoomed-in");
+});
+
