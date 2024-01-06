e29c75fe372466db0d7288c858a2077169d949c1
Viniciusdacostasilva <135259286+Viniciusdacostasilva@users.noreply.github.com>
2024-01-05 23:57:15 -0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -12,24 +12,6 @@ image2.addEventListener("mouseleave", () => {
 });
 
 
-/*Efeito de zoom ao passar o mouse na iamgem*/
-const defaultImage = document.querySelector(".default-image");
-
-defaultImage.addEventListener("mousemove", (e) => {
-    const { left, top, width, height } = defaultImage.getBoundingClientRect();
-    const x = e.clientX - left;
-    const y = e.clientY - top;
-    const centerX = width / 2;
-    const centerY = height / 2;
-    const deltaX = (x - centerX) / centerX;
-    const deltaY = (y - centerY) / centerY;
-
-    defaultImage.style.transform = `scale(1.2) translate(${deltaX * 10}px, ${deltaY * 10}px)`;
-});
-
-defaultImage.addEventListener("mouseleave", () => {
-    defaultImage.classList.remove("zoomed-in");
-});
 
 //Botão do Duvidas
   
